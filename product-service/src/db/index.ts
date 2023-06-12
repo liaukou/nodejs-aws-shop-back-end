08bd1f66d33e737d7c6bd9156bae15dd2285a8e7
Andrei Liaukou <andrei_liaukou@epam.com>
2023-06-12 11:09:19 +0200

    Task 4.1

--- product-service/src/db/index.ts
+++ product-service/src/db/index.ts
@@ -1,5 +1,17 @@
 import prducts from './products.json'
 
+export type Product = {
+  id: string
+  title: string
+  description: string
+  price: number
+}
+
+export type Stock = {
+  product_id: string
+  count: number
+}
+
 export const getProducts = async () => prducts
 
 export const getProductById = async (id: string) =>
