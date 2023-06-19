46e9734c50a99e6e0fb1a76e29d877ee1dadb2b0
Andrei Liaukou <andrei_liaukou@epam.com>
2023-06-19 17:50:44 +0200

    Fix

--- import-service/src/handlers/importProductsFile.ts
+++ import-service/src/handlers/importProductsFile.ts
@@ -1,6 +1,5 @@
 import { APIGatewayProxyEvent } from 'aws-lambda'
 import AWS from 'aws-sdk'
-import { PutObjectRequest } from 'aws-sdk/clients/s3'
 import { constants as httpConstants } from 'http2'
 import { createResponse } from '../utils/apiUtils'
 
@@ -10,31 +9,20 @@ AWS.config.update({
 
 const s3 = new AWS.S3()
 
-const tempData = `The Legend of Zelda: Breath of the Wild,Embark on an epic adventure in the kingdom of Hyrule.,59.99
-Animal Crossing: New Horizons,Create your own virtual paradise on a deserted island.,49.99`
-
 export const handler = async (event: APIGatewayProxyEvent) => {
   const fileName = event.queryStringParameters?.name || 'products.csv'
   const bucketName = process.env.bucketName
   const objectKey = `uploaded/${fileName}`
 
   try {
-    const putParams: PutObjectRequest = {
-      Bucket: bucketName,
-      Key: objectKey,
-      Body: tempData,
-    }
-
     const params = {
       Bucket: bucketName,
       Key: objectKey,
       Expires: 600,
+      ContentType: 'text/csv',
     }
 
-    await s3.putObject(putParams).promise()
-    const url = await s3.getSignedUrlPromise('getObject', params)
-
-    console.log('importProductsFile file is written successfully')
+    const url = await s3.getSignedUrlPromise('putObject', params)
 
     return createResponse(httpConstants.HTTP_STATUS_OK, { url })
   } catch (error) {
